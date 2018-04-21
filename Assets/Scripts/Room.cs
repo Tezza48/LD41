c98e264c324de4ecf2ff98316ac1a7a4f0dd55e2
William Terry <Tezza48is@aol.com>
2018-04-21 04:48:23 +0100

    Blocking for game map

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    public List<GameObject> objectsInRoom;
+
+	// Use this for initialization
+	void Start () {
+        foreach (var item in objectsInRoom)
+        {
+            item.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (var item in objectsInRoom)
+        {
+            item.SetActive(true);
+        }
+    }
+}
